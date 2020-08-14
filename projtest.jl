01f921bdcda6d27f23185c652422a6b4f00626a6
Aaron Fenyes <afenyes@math.toronto.edu>
2020-08-14 02:43:33 +0200

    Graph inputs to RF
    D'oh! They're reflection-symmetric about pi/2.

--- projtest.jl
+++ projtest.jl
@@ -7,25 +7,29 @@ C2 = 0.1;
 C3 = 3/44.;
 C4 = 1/14.;
 
-function RF(u, N)
+function RF(u, N, use_taylor)
     for n in 1:N
-      sqrt_u = sqrt.(u)
-      pair = sqrt_u .* sqrt_u[[2,3,1]]
-      lambda = pair[1] + pair[2] + pair[3]
-      u = 0.25*(u + fill(lambda, 3))
+        sqrt_u = sqrt.(u)
+        pair = sqrt_u .* sqrt_u[[2,3,1]]
+        lambda = pair[1] + pair[2] + pair[3]
+        u = 0.25*(u + fill(lambda, 3))
     end
     avg = (u[1] + u[2] + u[3])/3
-    off = u - fill(avg, 3)
-    e2 = off[1] * off[2] - off[3] * off[3]
-    e3 = off[1] * off[2] * off[3]
-    return (1 + (C1*e2 - C2 - C3*e3)*e2 + C4*e3) / sqrt(avg)
+    if use_taylor
+        off = u - fill(avg, 3)
+        e2 = off[1] * off[2] - off[3] * off[3]
+        e3 = off[1] * off[2] * off[3]
+        return (1 + (C1*e2 - C2 - C3*e3)*e2 + C4*e3) / sqrt(avg)
+    else
+        return 1/sqrt(avg)
+    end
 end
 
-function my_F(phi, k, N = 12)
+function my_F(phi, k, N = 12, use_taylor = true)
     c = cos(phi)
     s = sin(phi)
     ks = k*s
-    return s * RF([c*c, 1 - ks*ks, 1], N)
+    return s * RF([c*c, 1 - ks*ks, 1], N, use_taylor)
 end
 
 # --- Peirce projection
@@ -41,9 +45,9 @@ end
 
 SQRT_1_2 = 1/sqrt(2);
 
-function my_peirce_proj(zeta, N)
+function my_peirce_proj(zeta, N = 12, use_taylor = true)
     angles = acos.(reim(cn_coords(zeta)))
-    return 0.5*[my_F(angles[1], SQRT_1_2, N), my_F(angles[2], SQRT_1_2, N)]
+    return 0.5*[my_F(angles[1], SQRT_1_2, N, use_taylor), my_F(angles[2], SQRT_1_2, N, use_tayor)]
 end
 
 function good_peirce_proj(zeta)
@@ -53,16 +57,18 @@ end
 
 # --- test ---
 
-function test_peirce_proj(N = 12)
+function test_peirce_proj(N = 12, use_taylor = true)
     mesh = LinRange(0, 1, 200)
-    my_z = [my_peirce_proj(zeta, N) for zeta in mesh]
+    my_z = [my_peirce_proj(zeta, N, use_taylor) for zeta in mesh]
     good_z = good_peirce_proj.(mesh)
     plot(mesh, [first.(my_z), first.(good_z)], legend = false)
 end
 
-function test_F(N = 12)
+function test_F(N = 12, use_taylor = true)
     mesh = LinRange(0, pi, 200)
-    my_z = [my_F(phi, 1/sqrt(2), N) for phi in mesh]
+    my_z = [my_F(phi, 1/sqrt(2), N, use_taylor) for phi in mesh]
     good_z = [F(phi, 1/2) for phi in mesh]
-    plot(mesh, [first.(my_z), real.(good_z)], legend = false)
+    comparison = plot(mesh, [first.(my_z), real.(good_z)], legend = false)
+    crossover = plot(mesh, [cos.(mesh).^2, [1-0.5*sin(phi)^2 for phi in mesh]], legend = false)
+    plot(comparison, crossover, layout = (2, 1))
 end
